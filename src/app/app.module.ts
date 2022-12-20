eadf7ee56eac2e3326c3039fc39937f5f5d2a1c7
camotito0 <cecilianallerena@gmail.com>
2022-12-20 13:15:44 -0500

    setup ready, adding components card - filters and views dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    DashboardComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule
