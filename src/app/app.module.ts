e472abe646a62777c3697c66c44f4dde429c5252
camotito0 <cecilianallerena@gmail.com>
2022-12-20 20:20:50 -0500

    Adding bootstrap, reactive form for filters, adding searchCharacter function in dashboard view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { RickandmortyApiService } from 'src/app/services/rickandmorty-api.service';
 import { Character, Result } from '../../models/character';
 
@@ -10,14 +11,42 @@ import { Character, Result } from '../../models/character';
 export class DashboardComponent {
 
   characters: Result[] = [];
-  options: string[] = []
+  filteredCharacters: Result[] = [];
+  characterForm!: FormGroup;
 
-  constructor( public apiService:RickandmortyApiService) {}
+  constructor (
+    public apiService:RickandmortyApiService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.apiService.getCharacters().subscribe((resp:Character) => {
-      this.characters = resp.results
+      this.characters = resp.results;
+      this.filteredCharacters = [...this.characters]
     })
+
+    this.createForm();
+  }
+
+  private createForm() {
+    this.characterForm = this.fb.group({
+      characterName: [],
+      status: [],
+      gender: []
+    })
+  }
+  /* Rick Sanchez */
+
+  searchCharacter() {
+    console.log(this.characterForm.value)
+    let filterByName =  this.characterForm.value.characterName ? this.characters.filter(character => character.name === this.characterForm.value.characterName) :  this.characters;
+    let filterByGender = filterByName.filter(character => character.gender === this.characterForm.value.gender);
+    let filterByStatus = filterByGender.filter(character => character.status === this.characterForm.value.status);
+    this.filteredCharacters = filterByStatus
+  }
+
+  cleanForm() {
+    this.characterForm.reset()
   }
 
 }
